7d8fc6163db9622f026d7eb25bb2c82405b41352
Chintan <chintan.170410116089@gmail.com>
2023-01-29 12:50:26 +0530

    storyboard-and-interface project init

---
+++ storyboard-and-interface/storyboard-and-interface/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  storyboard-and-interface
+//
+//  Created by xscotophilic on 29/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
