1be43b049f325ef594945a61010a0cdc1bcc41a8
Chintan <chintan.170410116089@gmail.com>
2023-05-12 01:06:02 +0530

    Added: Responsive calculator app ui

---
+++ auto-layout-and-responsive-ui/calculator/calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by xscotophilic on 02/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
